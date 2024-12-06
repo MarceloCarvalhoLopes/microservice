f008ab01ad33cfe6d1621f69f042a2333fc70dcd
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2024-12-06 18:32:37 -0300

    Microservice Credit Assessor

---
+++ mscreditassessor/src/main/java/io/github/cursoms/mscreditassessor/MicroserviceCreditAssessorApplication.java
@@ -0,0 +1,13 @@
+package io.github.cursoms.mscreditassessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCreditAssessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCreditAssessorApplication.class, args);
+	}
+
+}
---
+++ mscreditassessor/src/test/java/io/github/cursoms/mscreditassessor/MicroserviceCreditAssessorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cursoms.mscreditassessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCreditAssessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
